10809ef7a699a72619d6c7fe99a60d442c8b5b81
Duc Anh Pham Le <anh.pham1@ntq-solution.com.vn>
2020-07-13 10:47:08 +0700

    export report

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,9 +12,17 @@ import java.util.Scanner;
 
 public class Main {
 
-        private final String url = "jdbc:postgresql://localhost:5432/timetable_test";
-        private final String user = "postgres";
-        private final String password = "12345";
+        private final String url = "jdbc:sqlserver://localhost:1433;databaseName=DBTestLocal";
+        private final String user = "sa";
+        private final String password = "123456";
+        private final static String filePath ="C:\\Users\\NTQ\\Downloads\\channel_member_only_mysql.csv";
+        private final static String query = "select *\n" +
+                "                    from community_service.channel_member cm\n" +
+                "                    join community_service.channel c ON cm.channel_id = c.id \n" +
+                "                    INNER JOIN community_service.`member` m ON m.id = cm.member_id \n" +
+                "                    INNER JOIN community_service.community c2 ON c.community_id = c2.id \n" +
+                "              \t\t\n" +
+                "                    where (c2.user_service_tenant_id = ? and cm.channel_id = ? and m.external_reference_id = ? and cm.joined =?) ";
 
         /**
          * Connect to the PostgreSQL database
@@ -25,42 +33,55 @@ public class Main {
             Connection conn = null;
             try {
                 conn = DriverManager.getConnection(url, user, password);
-                System.out.println("Connected to the PostgreSQL server successfully.");
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
 
             return conn;
         }
-
-        /**
-         * @param args the command line arguments
-         */
-        public static void main(String[] args) {
-            Main app = new Main();
-            try (Scanner scanner = new Scanner(new File("D:\\Project\\schedule\\people.csv"));) {
+        public void saveDataFromCSVToDB() throws SQLException {
+            String sql = "INSERT INTO dataRedshift (id) values (?)";
+            PreparedStatement statement = connect().prepareStatement(sql);
+            try (Scanner scanner = new Scanner(new File(filePath));) {
                 while (scanner.hasNextLine()) {
-
-                    String sql = "INSERT INTO data1 (id) values (?)";
-                    PreparedStatement statement = app.connect().prepareStatement(sql);
-                    statement.setInt(1, Integer.parseInt(app.getRecordFromLine(scanner.nextLine())));
+                    statement.setString(1,getRecordFromLine(scanner.nextLine()));
+                    System.out.println("Added data!");
                     statement.executeUpdate();
-                    statement.close();
-                    app.connect().close();
+
                 }
             } catch (FileNotFoundException | SQLException e) {
                 e.printStackTrace();
             }
         }
-    private String getRecordFromLine(String line) {
+        private String getRecordFromLine(String line) {
 
-        try (Scanner rowScanner = new Scanner(line)) {
-            while (rowScanner.hasNext()) {
-                return rowScanner.next();
+            try (Scanner rowScanner = new Scanner(line)) {
+                while (rowScanner.hasNext()) {
+                    return rowScanner.next();
+                }
+            }
+           return "";
+        }
+        private void generateReport() throws SQLException {
+            PreparedStatement statement = connect().prepareStatement(query);
+            try (Scanner scanner = new Scanner(new File(filePath))) {
+                while (scanner.hasNextLine()) {
+                    String [] values = getRecordFromLine(scanner.nextLine()).split(",");
+                    statement.setString(1,values[0].trim());
+                    statement.setString(2,values[1].trim());
+                    statement.setString(3,values[2].trim());
+                    statement.setString(4,values[3].trim());
+                    System.out.println(statement.toString());
+                }
+
+            } catch (FileNotFoundException | SQLException e) {
+                e.printStackTrace();
             }
         }
-       return "";
-    }
+        public static void main(String[] args) throws SQLException {
+            Main app = new Main();
+            app.generateReport();
+        }
 
-    }
+}
 
